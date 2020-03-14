4e8e49f0b7847491fe08cf2b4f89b15bb78dbac6
JamesYin <3401754684@qq.com>
2020-03-14 19:40:03 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="main">
+        <!-- 头部 -->
+        <Header :title="title"></Header>
+
+        <!-- 登陆部分 -->
+        <div id="content">
+            <Login></Login>
+        </div>
+        
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+import Login from '@/components/Login'
+export default {
+    name: 'Mine',
+    data() {
+        return {
+            title: '我的喵喵'
+        }
+    },
+    components: {
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
